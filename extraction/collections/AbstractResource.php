247ceffd7b2b9b49a194b319fa6790e8679e9563
Swapnil Srivastava <142904704+Swapnilden@users.noreply.github.com>
2024-04-04 22:19:47 +0530

    Refactor AbstractResource class
    - Fix typo in property name ($wasInititialized -> $wasInitialized)
    - Validate input parameter in constructor to ensure non-empty ID
    - Change visibility of $id property to protected
    - Declare class as abstract since it contains abstract method(s)
    - Add error handling in close() method for resource cleanup
    - Improve readability and clarity of code

--- extraction/collections/AbstractResource.php
+++ extraction/collections/AbstractResource.php
@@ -1,27 +1,25 @@
 <?php
 
 abstract class AbstractResource {
-	
-		protected $wasInititialized = false;
-		public $id;
-		
-		public function __construct($id){
-			$this->id = $id;
-			}
-		
-		/*
-		 * Connections should be bound to the lifetime of the object
-		 * */
-		public function __destruct(){
-				$this->close();
-			}
-		
-		public abstract function close();
-			
-		public function log ($lvl, $message){
-		
-			Logger::logComponent("resource",get_class($this)."", $lvl ,$message);
-		
-		}
-	
-	}
+    protected $wasInitialized = false;
+    protected $id;
+
+    public function __construct($id) {
+        if (!empty($id)) {
+            $this->id = $id;
+        } else {
+            throw new InvalidArgumentException('ID cannot be empty');
+        }
+    }
+
+    public function __destruct() {
+        $this->close();
+    }
+
+    public abstract function close();
+
+    protected function log($lvl, $message) {
+        Logger::logComponent("resource", get_class($this), $lvl, $message);
+    }
+}
+?>
